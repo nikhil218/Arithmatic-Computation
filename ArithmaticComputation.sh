9912eac767ec2f8f5ba375b7606db2c01cd39fe0
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-22 16:08:02 +0530

    Query 4

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -8,6 +8,8 @@ echo $a $b $c
 Query1=$(( $a + $b * $c ))
 Query2=$(( $a * $b + $c ))
 Query3=`echo - | awk '{printf "%.2f\n", '$c+$a/$b'}'`
+Query4=`echo - | awk '{printf "%.2f\n", '$a%$b+$c'}'`
 echo $Query1
 echo $Query2
 echo $Query3
+echo $Query4
